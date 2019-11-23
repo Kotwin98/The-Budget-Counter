bb5cf2ab66294c9d04a911cf4e98e50e05ee6e7d
Mateusz Kotwiński <mkotwinski98@gmail.com>
2019-11-23 13:32:07 +0100

    add addItem function

--- app.js
+++ app.js
@@ -22,7 +22,33 @@ var budgetController = (function(){
             exp: 0,
             inc: 0
         }
-    }
+    };
+
+    return {
+        addItem: function(type, des, val){
+            var newItem, ID;
+
+            //Create new id
+            if(data.allItems[type].length > 0){
+                ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+            } else{
+                ID = 0;
+            }
+
+            //Create new item based on 'inc' or 'exp' type
+            if(type === 'exp'){
+                newItem = new Expense(ID, des, val);
+            } else if(type === 'inc'){
+                newItem = new Income(ID, des, val)
+            }
+
+            //Push it into uor data structure
+            data.allItems[type].push(newItem);
+
+            //Return new element
+            return newItem;
+        } 
+    };
 
 })();
 
@@ -72,11 +98,13 @@ var controller = (function(budgetCtrl, UICtrl){
     }
 
     var ctrlAddItem = function(){
+        var input, newItem;
 
         // 1. Get the field input data
-        var input = UICtrl.getInput();
+        input = UICtrl.getInput();
 
         // 2. Add the item to the budget controller
+        newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
         // 3. Add the item to the UI
 
