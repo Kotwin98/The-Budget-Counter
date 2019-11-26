54cafaadb7227ebcf529e73c91269d30ac967555
Mateusz Kotwiński <mkotwinski98@gmail.com>
2019-11-26 09:56:26 +0100

    add clear fields function

--- app.js
+++ app.js
@@ -100,6 +100,21 @@ var UIController = (function(){
 
         },
 
+        clearFields: function(){
+            var fields, fieldsArr;
+
+            fields = document.querySelectorAll(DOMStrings.inputDescription + ', ' + DOMStrings.inputValue);
+
+            fieldsArr = Array.prototype.slice.call(fields);
+
+            fieldsArr.forEach(function(current, index, array){
+                current.value = "";
+            });
+            
+            fieldsArr[0].focus();
+
+        },
+
         getDOMStrings: function(){
             return DOMStrings;
         }
@@ -136,9 +151,12 @@ var controller = (function(budgetCtrl, UICtrl){
         // 3. Add the item to the UI
         UICtrl.addListItem(newItem, input.type);
 
-        // 4. Calculate the budget
+        // 4. Clear the fields
+        UICtrl.clearFields();
 
-        // 5. Display the budget on the UI
+        // 5. Calculate the budget
+
+        // 6. Display the budget on the UI
     
     }
 
