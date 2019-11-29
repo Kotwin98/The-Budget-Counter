9fe21921c8aec1e11e63e50c72455873a8ee6a8d
Mateusz Kotwiński <mkotwinski98@gmail.com>
2019-11-29 14:29:00 +0100

    add displayMonth function

--- app.js
+++ app.js
@@ -149,7 +149,8 @@ var UIController = (function(){
         expensesLabel: '.budget__expenses--value',
         percentageLabel: '.budget__expenses--percentage',
         container: '.container',
-        expensesPercLabel: '.item__percentage'
+        expensesPercLabel: '.item__percentage',
+        dateLabel: '.budget__title--month'
     }
 
     var formatNumber = function(num, type){
@@ -267,6 +268,21 @@ var UIController = (function(){
 
         },
 
+        displayMonth: function(){
+            var now, year, month, months;
+
+            now = new Date();
+
+            months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+            month = now.getMonth();
+
+            year = now.getFullYear();
+
+            document.querySelector(DOMStrings.dateLabel).textContent = months[month] + ' ' + year;
+            
+        },
+
         getDOMStrings: function(){
             return DOMStrings;
         }
@@ -375,6 +391,7 @@ var controller = (function(budgetCtrl, UICtrl){
     return {
         init: function(){
             console.log('Application has started');
+            UICtrl.displayMonth();
             UICtrl.displayBudget({
                 budget: 0,
                 totalInc: 0,
